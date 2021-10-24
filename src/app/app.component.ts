f4be9d2d52979cc2d5815cd60180d15833b6932a
Fabio <fabioaoad@gmail.com>
2021-10-24 18:35:09 -0300

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
+  
+  nombre: string = 'fabiO aoAd';
+  valor: number = 1000000;
+  obj = {
+    nombre: 'Fabio'
+  }
+  
+  mostrarNombre(){
+    console.log( this.nombre );
+    console.log( this.valor );
+    console.log( this.obj );
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+
 import { AppComponent } from './app.component';
+
+//Módulo personalizado
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PrimeNgModule
   ],
   providers: [],
   bootstrap: [AppComponent]
